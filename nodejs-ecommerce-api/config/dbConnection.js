679d195b18079839edb643f1f68667cab0f8cdc3
Raqib-Yoon <raqibyoon2020@gmail.com>
2024-01-20 21:36:28 +0430

    Mongo db connnection updated
    Mongo db connnection updated

--- nodejs-ecommerce-api/config/dbConnection.js
+++ nodejs-ecommerce-api/config/dbConnection.js
@@ -4,6 +4,7 @@ const dbConnect = () => {
   try {
     mongoose.connect(process.env.DATABASE);
     console.log("db connected successfull...");
+    mongoose.set("strictPopulate", false);
   } catch (error) {
     console.log(error.message);
   }
