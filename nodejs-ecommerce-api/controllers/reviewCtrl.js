f1e78890a2936dc6f681b20fde505763ffa464ec
Raqib-Yoon <raqibyoon2020@gmail.com>
2024-01-20 21:45:51 +0430

    review controller updated
    review controller updated

--- nodejs-ecommerce-api/controllers/reviewCtrl.js
+++ nodejs-ecommerce-api/controllers/reviewCtrl.js
@@ -4,12 +4,25 @@ import Product from "../model/Product.js";
 export const createReview = async (req, res) => {
   const { rating, message } = req.body;
   try {
-    const productExist = await Product.findById(req.params.productId);
+    const productExist = await Product.findById(req.params.productId).populate(
+      "reviews"
+    );
     if (!productExist) {
       return res.status(400).json({
         error: "Product not found.",
       });
     }
+    const hasReviewed = await productExist?.reviews?.find((review) => {
+      return review?.user.toString() === req?.userAuthId.toString();
+    });
+    console.log(hasReviewed);
+
+    if (hasReviewed) {
+      return res.status(400).json({
+        error: "Product has already been reviewed",
+      });
+    }
+
     const createdReview = await Review.create({
       message,
       rating,
