432c4d0f5dfc2a164153af6d5a695600a255e930
Raqib-Yoon <raqibyoon2020@gmail.com>
2024-01-20 21:47:27 +0430

    updated product router
    updated product router

--- nodejs-ecommerce-api/routes/productRoutes.js
+++ nodejs-ecommerce-api/routes/productRoutes.js
@@ -2,8 +2,8 @@ import express, { Router } from "express";
 const productRouter = express.Router();
 import {
   createProductCtrl,
-  getProductsCtrl,
-  getProductCtrl,
+  getAllProductsCtrl,
+  getSingleProductCtrl,
   updateProductCtrl,
   deleteProductCtrl,
 } from "../controllers/productsCtrl.js";
@@ -11,8 +11,8 @@ import isLoggedIn from "../middlewares/isLoggedIn.js";
 
 // product CRUD routes
 productRouter.post("/", isLoggedIn, createProductCtrl);
-productRouter.get("/", getProductsCtrl);
-productRouter.get("/:id", getProductCtrl);
+productRouter.get("/", getAllProductsCtrl);
+productRouter.get("/:id", getSingleProductCtrl);
 productRouter.put("/update/:id", isLoggedIn, updateProductCtrl);
 productRouter.delete("/delete/:id", isLoggedIn, deleteProductCtrl);
 
