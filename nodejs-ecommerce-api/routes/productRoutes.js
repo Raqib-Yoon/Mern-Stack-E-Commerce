9ff8effc53f4432c316f4130bdf02e25cd6e06f2
Raqib-Yoon <raqibyoon2020@gmail.com>
2024-02-08 11:43:12 +0430

    Update productRoutes.js

--- nodejs-ecommerce-api/routes/productRoutes.js
+++ nodejs-ecommerce-api/routes/productRoutes.js
@@ -8,12 +8,20 @@ import {
   deleteProductCtrl,
 } from "../controllers/productsCtrl.js";
 import isLoggedIn from "../middlewares/isLoggedIn.js";
+import upload from "../config/uploadFiles.js";
+import isAdmin from "../middlewares/isAdmin.js";
 
 // product CRUD routes
-productRouter.post("/", isLoggedIn, createProductCtrl);
+productRouter.post(
+  "/",
+  isLoggedIn,
+  isAdmin,
+  upload.single("file"),
+  createProductCtrl
+);
 productRouter.get("/", getAllProductsCtrl);
 productRouter.get("/:id", getSingleProductCtrl);
-productRouter.put("/update/:id", isLoggedIn, updateProductCtrl);
-productRouter.delete("/delete/:id", isLoggedIn, deleteProductCtrl);
+productRouter.put("/update/:id", isLoggedIn, isAdmin, updateProductCtrl);
+productRouter.delete("/delete/:id", isLoggedIn, isAdmin, deleteProductCtrl);
 
 export default productRouter;
