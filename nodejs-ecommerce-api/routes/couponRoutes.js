ec6933f57a2d75244056263c25e23937e3eee5ee
Raqib-Yoon <raqibyoon2020@gmail.com>
2024-02-08 11:43:05 +0430

    Update couponRoutes.js

--- nodejs-ecommerce-api/routes/couponRoutes.js
+++ nodejs-ecommerce-api/routes/couponRoutes.js
@@ -8,11 +8,12 @@ import {
   deleteCoupon,
 } from "../controllers/couponsCtrl.js";
 import isLoggedIn from "../middlewares/isLoggedIn.js";
+import isAdmin from "../middlewares/isAdmin.js";
 
-couponRouter.post("/", isLoggedIn, createCoupon);
+couponRouter.post("/", isLoggedIn, isAdmin, createCoupon);
 couponRouter.get("/", getAllCoupons);
 couponRouter.get("/:id", getSingleCoupon);
-couponRouter.put("/update/:id", updateCoupon);
-couponRouter.delete("/delete/:id", deleteCoupon);
+couponRouter.put("/update/:id", isLoggedIn, isAdmin, updateCoupon);
+couponRouter.delete("/delete/:id", isLoggedIn, isAdmin, deleteCoupon);
 
 export default couponRouter;
