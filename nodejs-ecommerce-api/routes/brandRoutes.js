97fcd04024a69621f4b99f1547a034353d773f8e
Raqib-Yoon <raqibyoon2020@gmail.com>
2024-02-08 11:42:54 +0430

    Update brandRoutes.js

--- nodejs-ecommerce-api/routes/brandRoutes.js
+++ nodejs-ecommerce-api/routes/brandRoutes.js
@@ -7,12 +7,13 @@ import {
   updateBrand,
 } from "../controllers/brandsCtrl.js";
 import isLoggedIn from "../middlewares/isLoggedIn.js";
+import isAdmin from "../middlewares/isAdmin.js";
 const brandRouter = express.Router();
 
-brandRouter.post("/", isLoggedIn, createBrand);
+brandRouter.post("/", isLoggedIn, isAdmin, createBrand);
 brandRouter.get("/", getAllBrands);
 brandRouter.get("/:id", getSingleBrand);
-brandRouter.put("/update/:id", updateBrand);
-brandRouter.delete("/delete/:id", deleteBrand);
+brandRouter.put("/update/:id", isLoggedIn, isAdmin, updateBrand);
+brandRouter.delete("/delete/:id", isLoggedIn, isAdmin, deleteBrand);
 
 export default brandRouter;
