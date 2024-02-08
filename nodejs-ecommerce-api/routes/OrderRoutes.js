93690cfd1f3399c634126832de88d6b5e9fe905b
Raqib-Yoon <raqibyoon2020@gmail.com>
2024-02-08 11:43:08 +0430

    Update OrderRoutes.js

--- nodejs-ecommerce-api/routes/OrderRoutes.js
+++ nodejs-ecommerce-api/routes/OrderRoutes.js
@@ -1,9 +1,11 @@
 import express from "express";
 const orderRouter = express.Router();
 import isLoggedIn from "../middlewares/isLoggedIn.js";
-import { createOrder } from "../controllers/OrderCtrl.js";
+import { createOrder, getSalesSum } from "../controllers/orderCtrl.js";
+import isAdmin from "../middlewares/isAdmin.js";
 
 // Order Routes
-orderRouter.post("/", isLoggedIn, createOrder);
+orderRouter.post("/", isLoggedIn, isAdmin, createOrder);
+orderRouter.get("/sales/sum", isLoggedIn, getSalesSum);
 
 export default orderRouter;
