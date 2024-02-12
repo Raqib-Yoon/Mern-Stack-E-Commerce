1522f19ddc9fed1cc4bba21a2de0460c91628d78
Raqib-Yoon <raqibyoon2020@gmail.com>
2024-02-12 21:31:34 +0430

    Update categoryRoutes.js

--- nodejs-ecommerce-api/routes/categoryRoutes.js
+++ nodejs-ecommerce-api/routes/categoryRoutes.js
@@ -8,9 +8,10 @@ import {
 } from "../controllers/categoriesCtrl.js";
 import isLoggedIn from "../middlewares/isLoggedIn.js";
 import isAdmin from "../middlewares/isAdmin.js";
+import catetgoryFileUpload from "../config/categoryUpload.js";
 const categoryRouter = express.Router();
 
-categoryRouter.post("/", isLoggedIn, isAdmin, createCategory);
+categoryRouter.post("/", isLoggedIn, isAdmin,catetgoryFileUpload.single("file"),createCategory);
 categoryRouter.get("/", getAllCategories);
 categoryRouter.get("/:id", getSingleCategory);
 categoryRouter.put("/update/:id", isLoggedIn, isAdmin, updateCategory);
