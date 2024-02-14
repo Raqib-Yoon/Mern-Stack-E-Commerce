0f3f65ac411277a777544ddd002c29dd636c1c0c
Raqib-Yoon <raqibyoon2020@gmail.com>
2024-02-15 00:44:05 +0430

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,25 @@
 import React from "react";
-
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 const App = () => {
+  const getProducts = async () => {
+    const response = await axios(`${baseURL}/products`);
+    return response.data;
+  };
+  const {
+    isLoading,
+    isError,
+    data: Products,
+  } = useQuery({ queryKey: ["Products"], queryFn: getProducts });
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+  if (isError) {
+    return <div>{data.error}</div>;
+  }
   return (
     <>
-      <h1 className="text-3xl font-bold underline text">Hello world!</h1>
+      <p>{Products}</p>
     </>
   );
 };
